7a84d031438f4eaf8446b097b3aa385bba746fe2
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-17 11:52:22 +0100

    first configurations for the backend

---
+++ server/src/index.ts
@@ -0,0 +1,19 @@
+import express from "express";
+import dotenv from "dotenv";
+import bodyParser from "body-parser";
+import cors from "cors";
+import helmet from "helmet";
+import morgan from "morgan";
+
+//route import
+
+//configurations
+dotenv.config();
+const app = express();
+app.use(express.json());
+app.use(helmet());
+app.use(helmet.crossOriginResourcePolicy({ policy: "cross-origin" }));
+app.use(morgan("common"));
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());

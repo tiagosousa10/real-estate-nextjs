0a17ca6adab89713352fd4498cc1b94372ff82f9
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-18 13:10:37 +0100

    create Header and signin footer components for authentication

--- client/src/app/(auth)/authProvider.tsx
+++ client/src/app/(auth)/authProvider.tsx
@@ -1,7 +1,12 @@
 import React from "react";
 import { Amplify } from "aws-amplify";
 
-import { Authenticator, useAuthenticator } from "@aws-amplify/ui-react";
+import {
+  Authenticator,
+  Heading,
+  useAuthenticator,
+  View,
+} from "@aws-amplify/ui-react";
 import "@aws-amplify/ui-react/styles.css";
 
 Amplify.configure({
@@ -14,11 +19,101 @@ Amplify.configure({
   },
 });
 
+const formFields = {
+  signIn: {
+    username: {
+      placeholder: "Enter your email",
+      label: "Email",
+      isRequired: true,
+    },
+    password: {
+      placeholder: "Enter your password",
+      label: "Password",
+      isRequired: true,
+    },
+  },
+  signUp: {
+    username: {
+      order: 1,
+      placeholder: "Choose a username",
+      label: "Username",
+      isRequired: true,
+    },
+    email: {
+      order: 2,
+      placeholder: "Enter your email address",
+      label: "Email",
+      isRequired: true,
+    },
+    password: {
+      order: 3,
+      placeholder: "Create a password",
+      label: "Password",
+      isRequired: true,
+    },
+    confirm_password: {
+      order: 4,
+      placeholder: "Confirm your password",
+      label: "Confirm Password",
+      isRequired: true,
+    },
+  },
+};
+
+const components = {
+  Header() {
+    return (
+      <View className="mt-4 mb-7">
+        <Heading level={3} className="!text-2xl !font-bold">
+          RENT
+          <span className="text-secondary-500 font-light hover:!text-primary-300 ">
+            IFUL
+          </span>
+        </Heading>
+        <p className="text-muted-foreground mt-2">
+          <span>Welcome!</span> Please sign in to continue
+        </p>
+      </View>
+    );
+  },
+  SignIn: {
+    Footer() {
+      const { toSignUp } = useAuthenticator();
+      return (
+        <View className="text-center mt-4">
+          <p className="text-muted-foreground">
+            Don&apos;t have an account?{" "}
+            <button
+              onClick={toSignUp}
+              className="text-primary hover:underline bg-transparent border-none p-0 "
+            >
+              Sign up here
+            </button>
+          </p>
+        </View>
+      );
+    },
+  },
+  //   SignUp: {
+  //    FormFields() {
+  //       const {validationErrors} = useAuthenticator();
+
+  //       return (
+  //          <>
+
+  //          </>
+  //       )
+  //    }
+  //   }
+};
+
 const Auth = ({ children }: { children: React.ReactNode }) => {
   const { user } = useAuthenticator((context) => [context.user]);
   return (
     <div className="h-full">
-      <Authenticator>{() => <>{children}</>}</Authenticator>
+      <Authenticator components={components} formFields={formFields}>
+        {() => <>{children}</>}
+      </Authenticator>
     </div>
   );
 };
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,6 +1,8 @@
 import Navbar from "@/components/Navbar";
+// import { redirect } from "next/navigation";
 
 export default function Home() {
+  // redirect("/landing");
   return (
     <div className="h-full w-full">
       <Navbar />

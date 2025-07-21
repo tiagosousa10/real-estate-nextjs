ec50651e37d078cf5ac75f56e8faa89a36be8892
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-21 13:26:18 +0100

    configure authentication for signup with aws

--- client/src/app/(auth)/authProvider.tsx
+++ client/src/app/(auth)/authProvider.tsx
@@ -4,6 +4,8 @@ import { Amplify } from "aws-amplify";
 import {
   Authenticator,
   Heading,
+  Radio,
+  RadioGroupField,
   useAuthenticator,
   View,
 } from "@aws-amplify/ui-react";
@@ -94,17 +96,43 @@ const components = {
       );
     },
   },
-  //   SignUp: {
-  //    FormFields() {
-  //       const {validationErrors} = useAuthenticator();
+  SignUp: {
+    FormFields() {
+      const { validationErrors } = useAuthenticator();
 
-  //       return (
-  //          <>
-
-  //          </>
-  //       )
-  //    }
-  //   }
+      return (
+        <>
+          <Authenticator.SignUp.FormFields />
+          <RadioGroupField
+            legend="Role"
+            name="custom:role"
+            errorMessage={validationErrors["custom:role"]}
+            hasError={!!validationErrors["custom:role"]}
+            isRequired
+          >
+            <Radio value="tenant">Tenant</Radio>
+            <Radio value="manager">Manager</Radio>
+          </RadioGroupField>
+        </>
+      );
+    },
+    Footer() {
+      const { toSignIn } = useAuthenticator();
+      return (
+        <View className="text-center mt-4">
+          <p className="text-muted-foreground">
+            Don&apos;t have an account?{" "}
+            <button
+              onClick={toSignIn}
+              className="text-primary hover:underline bg-transparent border-none p-0 "
+            >
+              Sign in
+            </button>
+          </p>
+        </View>
+      );
+    },
+  },
 };
 
 const Auth = ({ children }: { children: React.ReactNode }) => {
--- client/src/app/page.tsx
+++ client/src/app/page.tsx
@@ -1,4 +1,5 @@
 import Navbar from "@/components/Navbar";
+import Landing from "./(nondashboard)/landing/page";
 // import { redirect } from "next/navigation";
 
 export default function Home() {
@@ -6,6 +7,9 @@ export default function Home() {
   return (
     <div className="h-full w-full">
       <Navbar />
+      <main className={`h-full flex w-full flex-col`}>
+        <Landing />
+      </main>
     </div>
   );
 }

64b79a8d7ff00f0ea2de4e20be0b48ed7537a65f
tiagosousa <tiagosousa.tams@hotmail.com>
2025-07-29 17:50:38 +0100

    create mutation to update tenant settings

--- client/src/state/api.ts
+++ client/src/state/api.ts
@@ -18,7 +18,7 @@ export const api = createApi({
     },
   }),
   reducerPath: "api",
-  tagTypes: [],
+  tagTypes: ["Managers", "Tenants"],
   endpoints: (build) => ({
     getAuthUser: build.query<User, void>({
       queryFn: async (_, _queryApi, _extraoptions, fetchWithBQ) => {
@@ -61,7 +61,19 @@ export const api = createApi({
         }
       },
     }),
+
+    updateTenantSettings: build.mutation<
+      Tenant,
+      { cognitoId: string } & Partial<Tenant>
+    >({
+      query: ({ cognitoId, ...updateTenant }) => ({
+        url: `/tenants/${cognitoId}`,
+        method: "PUT",
+        body: updateTenant,
+      }),
+      invalidatesTags: (result) => [{ type: "Tenants", id: result?.id }],
+    }),
   }),
 });
 
-export const { useGetAuthUserQuery } = api;
+export const { useGetAuthUserQuery, useUpdateTenantSettingsMutation } = api;

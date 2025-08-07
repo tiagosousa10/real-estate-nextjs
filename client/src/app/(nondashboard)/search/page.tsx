64c0bf11d35356008d2f19eb42084dce981e5c42
tiagosousa <tiagosousa.tams@hotmail.com>
2025-08-07 19:45:13 +0100

    create search page and first functions with redux to set and toggle filters

---
+++ client/src/app/(nondashboard)/search/page.tsx
@@ -0,0 +1,14 @@
+import { useAppDispatch, useAppSelector } from "@/state/redux";
+import { useSearchParams } from "next/navigation";
+import React from "react";
+
+const SearchPage = () => {
+  const searchParams = useSearchParams();
+  const dispatch = useAppDispatch();
+  const isFiltersFullOpen = useAppSelector(
+    (state) => state.global.isFiltersFullOpen
+  );
+  return <div>SearchPage</div>;
+};
+
+export default SearchPage;

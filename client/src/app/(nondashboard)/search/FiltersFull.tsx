5da4924eeb3022898b18491cf0460db986834bc6
Tiago Sousa <148981307+tiagosousa10@users.noreply.github.com>
2025-09-08 22:54:18 +0100

    handle location search

--- client/src/app/(nondashboard)/search/FiltersFull.tsx
+++ client/src/app/(nondashboard)/search/FiltersFull.tsx
@@ -62,6 +62,28 @@ const FiltersFull = () => {
         : [...prev.amenities, amenity],
     }));
   };
+  const handleLocationSearch = async () => {
+    try {
+      const response = await fetch(
+        `https://api.mapbox.com/geocoding/v5/mapbox.places/${encodeURIComponent(
+          localFilters.location
+        )}.json?access_token=${
+          process.env.NEXT_PUBLIC_MAPBOX_ACCESS_TOKEN
+        }&fuzzyMatch=true`
+      );
+
+      const data = await response.json();
+      if (data.features && data.features.length > 0) {
+        const [lng, lat] = data.features[0].center;
+        setLocalFilters((prev) => ({
+          ...prev,
+          coordinates: [lng, lat],
+        }));
+      }
+    } catch (error) {
+      console.error("Error search location:", error);
+    }
+  };
 
   if (!isFiltersFullOpen) return null;
 

dbd322a3c5b13bc86071f6138294a8899ad1e313
Tiago Sousa <148981307+tiagosousa10@users.noreply.github.com>
2025-09-09 18:53:28 +0100

    Property Overview first implementations and checks

---
+++ client/src/app/(nondashboard)/search/[id]/PropertyOverview.tsx
@@ -0,0 +1,45 @@
+import { useGetPropertyQuery } from "@/state/api";
+import { MapPin, Star } from "lucide-react";
+import React from "react";
+
+const PropertyOverview = ({ propertyId }: PropertyOverviewProps) => {
+  const {
+    data: property,
+    isLoading,
+    isError,
+  } = useGetPropertyQuery(propertyId);
+
+  if (isLoading) return <>Loading...</>;
+  if (isError || !property) {
+    return <>Property not found</>;
+  }
+
+  return (
+    <div>
+      <div className="mb-4">
+        <div className="text-sm to-gray-500 mb-1">
+          Indonesia / Bali /{" "}
+          <span className="font-semibold text-gray-600">South Kuta</span>
+        </div>
+        <h1 className="text-3xl font-bold my-5">{property.name}</h1>
+        <div className="flex justify-between items-center">
+          <span className="flex items-center text-gray-500">
+            <MapPin className="w-4 h-4 mr-1 text-gray-700" />
+            {property.location?.city}, {property.location?.state},{" "}
+            {property.location?.country}
+          </span>
+          <div className="flex justify-between items-center gap-3">
+            <span className="flex items-center text-yellow-500">
+              <Star className="w-4 h-4 mr-1 fill-current" />
+              {property.averageRating.toFixed(1)} ({property.numberOfReviews}{" "}
+              Reviews)
+            </span>
+            <span className="text-green-600">Verified Listing</span>
+          </div>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default PropertyOverview;
--- client/src/app/(nondashboard)/search/[id]/page.tsx
+++ client/src/app/(nondashboard)/search/[id]/page.tsx
@@ -3,6 +3,7 @@ import { useGetAuthUserQuery } from "@/state/api";
 import { useParams } from "next/navigation";
 import React from "react";
 import ImagePreviews from "./ImagePreviews";
+import PropertyOverview from "./PropertyOverview";
 
 const SingleListing = () => {
   const { id } = useParams();
@@ -14,6 +15,11 @@ const SingleListing = () => {
       <ImagePreviews
         images={["/singlelisting-2.jpg", "/singlelisting-3.jpg"]}
       />
+      <div className="flex flex-col md:flex-row justify-center gap-10 mx-10 md:w-2/3 md:mx-auto mt-16 mb-8">
+        <div className="order-2 md:order-1">
+          <PropertyOverview propertyId={propertyId} />
+        </div>
+      </div>
     </div>
   );
 };

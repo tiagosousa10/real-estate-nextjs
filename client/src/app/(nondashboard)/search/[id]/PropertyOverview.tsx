6a3c2f23a568d0ffb16335d404ea7354a3a3be3b
Tiago Sousa <148981307+tiagosousa10@users.noreply.github.com>
2025-09-09 18:59:03 +0100

    create details for property overview

--- client/src/app/(nondashboard)/search/[id]/PropertyOverview.tsx
+++ client/src/app/(nondashboard)/search/[id]/PropertyOverview.tsx
@@ -16,10 +16,13 @@ const PropertyOverview = ({ propertyId }: PropertyOverviewProps) => {
 
   return (
     <div>
+      {/* header */}
       <div className="mb-4">
         <div className="text-sm to-gray-500 mb-1">
-          Indonesia / Bali /{" "}
-          <span className="font-semibold text-gray-600">South Kuta</span>
+          {property.location?.country} / {property.location?.state} /{" "}
+          <span className="font-semibold text-gray-600">
+            {property.location?.city}
+          </span>
         </div>
         <h1 className="text-3xl font-bold my-5">{property.name}</h1>
         <div className="flex justify-between items-center">
@@ -35,6 +38,36 @@ const PropertyOverview = ({ propertyId }: PropertyOverviewProps) => {
               Reviews)
             </span>
             <span className="text-green-600">Verified Listing</span>
+          </div>
+        </div>
+      </div>
+
+      {/* details */}
+      <div className="border border-primary-200 rounded-xl p-6 mb-6">
+        <div className="flex justify-between items-center gap-4 px-5">
+          <div>
+            <div className="text-sm text-gray-500">Monthly Rent</div>
+            <div className="font-semibold">
+              ${property.pricePerMonth.toLocaleString()}
+            </div>
+          </div>
+          <div className="border-l border-gray-300 h-10">
+            <div>
+              <div className="text-sm text-gray-500">Bedrooms</div>
+              <div className="font-semibold">{property.beds} bd</div>
+            </div>
+            <div className="border-l border-gray-300 h-10"></div>
+            <div>
+              <div className="text-sm text-gray-500">Bathrooms</div>
+              <div className="font-semibold">{property.baths} ba</div>
+            </div>
+            <div className="border-l border-gray-300 h-10"></div>
+            <div>
+              <div className="text-sm text-gray-500">Square Feet</div>
+              <div className="font-semibold">
+                {property.squareFeet.toLocaleString()} sq ft
+              </div>
+            </div>
           </div>
         </div>
       </div>

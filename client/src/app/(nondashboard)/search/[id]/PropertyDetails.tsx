68063b9ff9e20e7b9ee2409cffe23fbcf3af0f6d
Tiago Sousa <148981307+tiagosousa10@users.noreply.github.com>
2025-09-10 18:14:47 +0100

    highlights and tabs section created

--- client/src/app/(nondashboard)/search/[id]/PropertyDetails.tsx
+++ client/src/app/(nondashboard)/search/[id]/PropertyDetails.tsx
@@ -1,7 +1,8 @@
-import { AmenityIcons } from "@/lib/constants";
+import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
+import { AmenityIcons, HighlightIcons } from "@/lib/constants";
 import { formatEnumString } from "@/lib/utils";
 import { useGetPropertyQuery } from "@/state/api";
-import { HelpCircle, MapPin, Star } from "lucide-react";
+import { HelpCircle } from "lucide-react";
 import React from "react";
 
 const PropertyDetails = ({ propertyId }: PropertyDetailsProps) => {
@@ -38,6 +39,80 @@ const PropertyDetails = ({ propertyId }: PropertyDetailsProps) => {
             );
           })}
         </div>
+      </div>
+      {/* highlights */}
+      <div className="mt-12 mb-16">
+        <h3 className="text-xl font-semibold text-primary-800 dark:text-primary-100">
+          Highlights
+        </h3>
+        <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-3 lg:grid-cols-4 gap-8 mt-4 w-full">
+          {property.highlights.map((highlight: HighlightEnum) => {
+            const Icon =
+              HighlightIcons[highlight as HighlightEnum] || HelpCircle;
+            return (
+              <div
+                key={highlight}
+                className="flex flex-col items-center border rounded-xl py-8 px-4"
+              >
+                <Icon className="w-8 h-8 mb-2 text-primary-600 dark:text-primary-300" />
+                <span className="text-sm text-center text-primary-600 dark:text-primary-300">
+                  {formatEnumString(highlight)}
+                </span>
+              </div>
+            );
+          })}
+        </div>
+      </div>
+
+      {/* tabs section */}
+      <div>
+        <h3 className="text-xl font-semibold text-primary-800 dark:text-primary-100 mb-5">
+          Fees and Policies
+        </h3>
+        <p className="text-sm text-primary-600 dark:text-primary-300 mt-2">
+          The fees below are based on community-supplied data and may exclude
+          additional fees and utilities.
+        </p>
+        <Tabs defaultValue="required-fees" className="mt-8">
+          <TabsList className="grid w-full grid-cols-3">
+            <TabsTrigger value="required-fees">Required Fees</TabsTrigger>
+            <TabsTrigger value="pets">Pets</TabsTrigger>
+            <TabsTrigger value="parking">Parking</TabsTrigger>
+          </TabsList>
+          <TabsContent value="required-fees" className="w-1/3">
+            <p className="font-semibold mt-5 mb-2">One time move in fees</p>
+            <hr />
+            <div className="flex justify-between py-2 bg-secondary-50">
+              <span className="text-primary-700 font-medium">
+                Application Fee
+              </span>
+              <span className="text-primary-700">
+                ${property.applicationFee}
+              </span>
+            </div>
+            <hr />
+            <div className="flex justify-between py-2 bg-secondary-50">
+              <span className="text-primary-700 font-medium">
+                Security Deposit
+              </span>
+              <span className="text-primary-700">
+                ${property.securityDeposit}
+              </span>
+            </div>
+            <hr />
+          </TabsContent>
+          <TabsContent value="pets">
+            <p className="font-semibold mt-5 mb-2">
+              Pets are {property.isPetsAllowed ? "allowed" : "not allowed"}
+            </p>
+          </TabsContent>
+          <TabsContent value="parking">
+            <p className="font-semibold mt-5 mb-2">
+              Parking is{" "}
+              {property.isParkingIncluded ? "included" : "not included"}
+            </p>
+          </TabsContent>
+        </Tabs>
       </div>
     </div>
   );

4aeb31a278f4642f8daa6b04ed465a0ea1cf02ee
Tiago Sousa <148981307+tiagosousa10@users.noreply.github.com>
2025-09-30 17:43:13 +0100

    create current residences for tenants

--- client/src/app/(dashboard)/layout.tsx
+++ client/src/app/(dashboard)/layout.tsx
@@ -41,7 +41,7 @@ const DashboardLayout = ({ children }: { children: React.ReactNode }) => {
       <div className="min-h-screen w-full bg-primary-100">
         <Navbar />
         <div style={{ paddingTop: `${NAVBAR_HEIGHT}px` }}>
-          <main>
+          <main className="flex">
             <AppSidebar userType={authUser?.userRole.toLocaleLowerCase()} />
             <div className="flex-grow transition-all duration-300">
               {children}

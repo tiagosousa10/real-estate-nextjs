fdc656e86ae59cdee761758a24a4576640ff772f
Tiago Sousa <148981307+tiagosousa10@users.noreply.github.com>
2025-10-02 11:39:06 +0100

    update hero section with handle search location

--- client/src/app/(nondashboard)/search/[id]/page.tsx
+++ client/src/app/(nondashboard)/search/[id]/page.tsx
@@ -7,6 +7,7 @@ import PropertyOverview from "./PropertyOverview";
 import PropertyDetails from "./PropertyDetails";
 import PropertyLocation from "./PropertyLocation";
 import ContactWidget from "./ContactWidget";
+import ApplicationModal from "./ApplicationModal";
 
 const SingleListing = () => {
   const { id } = useParams();
@@ -29,6 +30,13 @@ const SingleListing = () => {
           <ContactWidget onOpenModal={() => setIsModalOpen(true)} />
         </div>
       </div>
+      {authUser && (
+        <ApplicationModal
+          isOpen={isModalOpen}
+          onClose={() => setIsModalOpen(false)}
+          propertyId={propertyId}
+        />
+      )}
     </div>
   );
 };

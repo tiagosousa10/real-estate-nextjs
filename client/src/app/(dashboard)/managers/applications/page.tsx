947ebf5b07094a2eadce055e1167f3fa4911c6ef
Tiago Sousa <148981307+tiagosousa10@users.noreply.github.com>
2025-10-02 10:54:44 +0100

    filter and map each application for the application card

--- client/src/app/(dashboard)/managers/applications/page.tsx
+++ client/src/app/(dashboard)/managers/applications/page.tsx
@@ -1,4 +1,5 @@
 "use client";
+import ApplicationCard from "@/components/ApplicationCard";
 import Header from "@/components/Header";
 import Loading from "@/components/Loading";
 import { Tabs, TabsContent, TabsList, TabsTrigger } from "@/components/ui/tabs";
@@ -7,6 +8,8 @@ import {
   useGetAuthUserQuery,
   useUpdateApplicationStatusMutation,
 } from "@/state/api";
+import { CircleCheckBig, Download, File, Hospital } from "lucide-react";
+import Link from "next/link";
 import React, { useState } from "react";
 
 const Applications = () => {
@@ -69,7 +72,102 @@ const Applications = () => {
                   tab === "all" || application.status.toLowerCase() === tab
               )
               .map((application) => (
-                <></>
+                <ApplicationCard
+                  key={application.id}
+                  application={application}
+                  userType="manager"
+                >
+                  <div className="flex justify-between gap-5 w-full pb-4 px-4">
+                    {/* Colored Section Status */}
+                    <div
+                      className={`p-4 text-green-700 grow ${
+                        application.status === "Approved"
+                          ? "bg-green-100"
+                          : application.status === "Denied"
+                          ? "bg-red-100"
+                          : "bg-yellow-100"
+                      }`}
+                    >
+                      <div className="flex flex-wrap items-center">
+                        <File className="w-5 h-5 mr-2 flex-shrink-0" />
+                        <span className="mr-2">
+                          Application submitted on{" "}
+                          {new Date(
+                            application.applicationDate
+                          ).toLocaleDateString()}
+                          .
+                        </span>
+                        <CircleCheckBig className="w-5 h-5 mr-2 flex-shrink-0" />
+                        <span
+                          className={`font-semibold ${
+                            application.status === "Approved"
+                              ? "text-green-800"
+                              : application.status === "Denied"
+                              ? "text-red-800"
+                              : "text-yellow-800"
+                          }`}
+                        >
+                          {application.status === "Approved" &&
+                            "This application has been approved."}
+                          {application.status === "Denied" &&
+                            "This application has been denied."}
+                          {application.status === "Pending" &&
+                            "This application is pending review."}
+                        </span>
+                      </div>
+                    </div>
+
+                    {/* Right Buttons */}
+                    <div className="flex gap-2">
+                      <Link
+                        href={`/managers/properties/${application.property.id}`}
+                        className={`bg-white border border-gray-300 text-gray-700 py-2 px-4 
+                          rounded-md flex items-center justify-center hover:bg-primary-700 hover:text-primary-50`}
+                        scroll={false}
+                      >
+                        <Hospital className="w-5 h-5 mr-2" />
+                        Property Details
+                      </Link>
+                      {application.status === "Approved" && (
+                        <button
+                          className={`bg-white border border-gray-300 text-gray-700 py-2 px-4
+                          rounded-md flex items-center justify-center hover:bg-primary-700 hover:text-primary-50`}
+                        >
+                          <Download className="w-5 h-5 mr-2" />
+                          Download Agreement
+                        </button>
+                      )}
+                      {application.status === "Pending" && (
+                        <>
+                          <button
+                            className="px-4 py-2 text-sm text-white bg-green-600 rounded hover:bg-green-500"
+                            onClick={() =>
+                              handleStatusChange(application.id, "Approved")
+                            }
+                          >
+                            Approve
+                          </button>
+                          <button
+                            className="px-4 py-2 text-sm text-white bg-red-600 rounded hover:bg-red-500"
+                            onClick={() =>
+                              handleStatusChange(application.id, "Denied")
+                            }
+                          >
+                            Deny
+                          </button>
+                        </>
+                      )}
+                      {application.status === "Denied" && (
+                        <button
+                          className={`bg-gray-800 text-white py-2 px-4 rounded-md flex items-center
+                          justify-center hover:bg-secondary-500 hover:text-primary-50`}
+                        >
+                          Contact User
+                        </button>
+                      )}
+                    </div>
+                  </div>
+                </ApplicationCard>
               ))}
           </TabsContent>
         ))}
